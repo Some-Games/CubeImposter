fd4f288ef697491bf182636323a6c23c57253400
Chris Christensen <chriscrossed.86@gmail.com>
2020-09-20 16:29:21 -0700

    First Successful Test

---
+++ Cube_Imposter/Assets/MakeInvisibleOnRun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRC;
+using VRCSDK2;
+
+public class MakeInvisibleOnRun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.GetComponent<Renderer>())
+            gameObject.GetComponent<Renderer>().enabled = false;
+
+        if(gameObject.transform.childCount > 0)
+            for(int i = 0; i < gameObject.transform.childCount; ++i)
+            {
+                GameObject tempChild = gameObject.transform.GetChild(i).gameObject;
+
+                if (tempChild.GetComponent<Renderer>())
+                    tempChild.GetComponent<Renderer>().enabled = false;
+            }
+
+    }
+}
